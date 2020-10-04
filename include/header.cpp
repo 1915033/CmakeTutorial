53e98e1dc1b63f5668c0a582d8c37296b029c32f
1915033 <53931783+1915033@users.noreply.github.com>
2020-10-04 20:37:37 +0530

    Just made a code bit more readable and easy to interpret
    I hope you like it .It is always better to add fewer comments to extend the readability of code also to describe the logic you are using.

--- include/header.cpp
+++ include/header.cpp
@@ -1,48 +1,54 @@
-#include<math.h>
+#include<math.h>  // #(preprocessor directive) include header file math.h
 
-int factorial(int num)
+//function to find factorial of a number:
+int factorial(int num) //definition of a function :syntax:return_type function_name(arguments)
 { int i,fact=1;
-  for( i=1; i<=num ; i++)
-   fact *= i;
+  for( i=1; i<=num ; i++) //forloop_syntax:for(initialization;condition;increment/decrement
+   fact *= i;    //recursion(calling a function itself again and again untill loop exits
   return fact;
 }
 
-char isprime(int num)
+//function to find whether a number is prime or not:
+char isprime(int number) //function definition
 { int i;
-  for( i=2; i<num ; i++ )
-   if( num%i == 0 )
-      return 'n';
-  if( i==num) 
-    return 'y';
+  for( i=2; i<number ; i++ )//for_loop
+   if( number%i == 0 )
+      return 'n'; //n=No number is not prime
+  if( i==number) 
+    return 'y';  //y=Yes number is prime
 
 }
 
-char ispalindrome(int num)
-{ int i, rev=0;
-  for( ; num!=0 ; num/=10 )
-   rev = rev*10 + num%10;
-  if(rev == num  )
-    return 'y';
+//function to check whether a number is palindrome or not:
+char ispalindrome(int number)   //function definition
+{ int i, reverse=0;
+  for( ; number!=0 ; number/=10 )  //  number/= 10 gives number containing all digits except the last. It's just a way of iterating over digits.
+n /= 10 is equivalent to n = n / 10 
+   reverse = reverse*10 + number%10;
+  if(reverse == number )    //palindrome number is a number if we reverse the digits the output is same
+    return 'y';  //y= yes number is a palindrome 
   else
-    return 'n';
+    return 'n';  //n= no, the number is not a palindrome
 }
 
-char isarmstrong(int num)
+//function to check whether a number is armstrong or not:
+char isarmstrong(int num)   
 { int i, arm=0, len=0, Digits[len], num_cpy;
-  for( num_cpy=num, i=0 ; num_cpy!=0 ; num_cpy/=10 )
-  { len++;
+  for( num_cpy=num, i=0 ; num_cpy!=0 ; num_cpy/=10 )//the value of number is passed to copied number to iterate that in loop ,initialization is from zero ,loop stops when number is not equal to 0
+  { len++;//len++means every time the loop is iterated the value of len increases by 1
     Digits[i] = num_cpy%10;
   }
-  for( i=0; i<len; i++ )
-   arm += pow(Digits[i],len);  
-  if( arm = num )
+  for( i=0; i<len; i++ )//for loop 
+   arm += pow(Digits[i],len);  // arm = arm + pow(Digits[i],len)     //pow(base,exponent) eg:pow(5,2)gives 5^2=25  
+  if( arm = num ) //if else condition
 	  return 'y';
   else 
 	  return 'n';
 }
 
-int average( int num1, int num2 )
-{ return (num1 + num2)/2;
+//function to find average of two numbers:
+int average( int num1, int num2 )    //function definition    //taking two numbers as argument/parametetrs
+{ return (num1 + num2)/2;  //returning average of two numbers
 }
 
 
